66ccb0045f0b8e84a1f269e2693ed4501d3673cf
9bany <bany.y0599@gmail.com>
2022-03-24 16:15:07 +0700

    composer-lib: fix bug

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# composer-lib
+# Component library starter template
+## Used libraries
+1. [React.js v17](https://reactjs.org/)
+2. [TypeScript](https://www.typescriptlang.org/)
+3. [Rollup.js](https://rollupjs.org/)
+4. [Storybook](https://storybook.js.org/)
+
--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,61 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import pkg from "./package.json";
+import typescript from 'rollup-plugin-typescript2';
 
-const packageJson = require("./package.json");
+const commonOutputOptions = {
+  dir: "dist",
+  preserveModules: true,
+  preserveModulesRoot: "src",
+};
 
-export default [
-    {
-        input: "src/index.ts",
-        output: [
-            {
-                file: packageJson.main,
-                format: "cjs",
-                sourcemap: true,
-            },
-            {
-                file: packageJson.module,
-                format: "esm",
-                sourcemap: true,
-            },
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: "./tsconfig.json" }),
-            postcss(), 
-        ],
-    },
-    {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: "dist/index.d.ts", format: "esm" }],
-        plugins: [dts()],
-        external: [/\.css$/],
-    },
-];
+function getOutputOptions(options) {
+  return {
+    ...commonOutputOptions,
+    ...options,
+  };
+}
+
+function withSourceMapOutputOptions(options) {
+  return {
+    ...options,
+    sourcemap: true,
+  };
+}
+
+function getCJSOutputOptions(options) {
+  return {
+    ...getOutputOptions(options),
+    format: "cjs",
+    exports: "named",
+  };
+}
+
+function getESMOutputOptions(options) {
+  return {
+    ...getOutputOptions(options),
+    format: "esm",
+  };
+}
+
+export default {
+  input: ["src/index.tsx"],
+  output: [
+    // ESM
+    getESMOutputOptions(
+      withSourceMapOutputOptions({
+        entryFileNames: "[name].esm.js",
+      })
+    ),
+    // CommonJS
+    getCJSOutputOptions(
+      withSourceMapOutputOptions({})
+    )
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript(),
+  ],
+  external: Object.keys(pkg.devDependencies),
+};
